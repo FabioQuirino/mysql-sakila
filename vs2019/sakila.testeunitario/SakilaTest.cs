65863458380f83423811948b82d417bfd3af1d5f
FabioQuirino <quirino@fqtecnologia.com.br>
2020-03-11 00:11:23 -0300

    teste

--- vs2019/sakila.testeunitario/SakilaTest.cs
+++ vs2019/sakila.testeunitario/SakilaTest.cs
@@ -10,7 +10,7 @@ namespace sakila.testeunitario
         public void TestarAcessoBd()
         {
             var teste = new servicoActor().Listar();
-            Assert.IsTrue(teste.Count > 1000, $"erro de teste. Encontrados: {teste.Count}");
+            Assert.IsTrue(teste.Count > 100, $"erro de teste. Encontrados: {teste.Count}");
         }
     }
 }
