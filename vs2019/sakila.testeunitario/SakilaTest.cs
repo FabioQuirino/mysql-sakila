e51534f5b98a5f98a14cb0bc314e1b12bb12dec9
FabioQuirino <quirino@fqtecnologia.com.br>
2020-03-10 23:45:11 -0300

    ajuste no teste unitário.

--- vs2019/sakila.testeunitario/SakilaTest.cs
+++ vs2019/sakila.testeunitario/SakilaTest.cs
@@ -10,6 +10,7 @@ namespace sakila.testeunitario
         public void TestarAcessoBd()
         {
             var teste = new servicoActor().Listar();
+            Assert.IsTrue(teste.Count > 1000, $"erro de teste. Encontrados: {teste.Count}");
         }
     }
 }
