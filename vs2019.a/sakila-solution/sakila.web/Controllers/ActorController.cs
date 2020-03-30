e99552d253d536e95062ebe8708584cf171322ac
FabioQuirino <quirino@fqtecnologia.com.br>
2020-03-30 15:16:25 -0300

    Inclsuão do projeto sakila.web.

---
+++ vs2019.a/sakila-solution/sakila.web/Controllers/ActorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sakila.web.Controllers
+{
+    public class ActorController : Controller
+    {
+        public IActionResult Index()
+        {
+            var servicoAtor = new sakila.repositorio.servico.ServicoActor();
+            var atores = servicoAtor.Listar();
+            return View(atores);
+        }
+    }
+}
