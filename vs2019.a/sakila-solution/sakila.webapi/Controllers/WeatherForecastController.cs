b4cdc220c5d66a6d0165d7b60fb160c39d2c8b53
FabioQuirino <quirino@fqtecnologia.com.br>
2020-04-21 19:43:40 -0300

    Inclsuão do projeto webapi.

---
+++ vs2019.a/sakila-solution/sakila.webapi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using sakila.model;
+using sakila.repositorio.servico;
+
+namespace sakila.webapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<actor> Get()
+        {
+            var atores = new ServicoActor().Listar();
+            return atores;
+        }
+
+    }
+}
